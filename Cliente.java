15c1ec1d86e8d49f892feaa94bc641103f2ccd70
Roberto Eugenio dos Santos <30935436+Robertoeugenio@users.noreply.github.com>
2018-08-22 16:16:14 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Cliente {
+
+	private String nome;
+	private int id;
+	private Double salario;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
